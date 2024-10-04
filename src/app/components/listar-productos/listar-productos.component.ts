bd537fce90971423ccedb39ff707dfe4612e08dc
Alex <alexnicolashoyos@gmail.com>
2024-10-03 22:46:57 -0300

    Cambio en listar

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../models/producto';
 
 @Component({
   selector: 'app-listar-productos',
@@ -9,6 +10,7 @@ import { ProductoService } from '../../services/producto.service';
 })
 export class ListarProductosComponent implements OnInit{
 
+  listarProducto : Producto[] = [];
   constructor(private _productoService: ProductoService  ) { }
 
   ngOnInit(): void {
@@ -18,6 +20,7 @@ export class ListarProductosComponent implements OnInit{
   obtenerProductos() {
     this._productoService.getProductos().subscribe( data => {
       console.log(data);
+      this.listarProducto = data;
     }, error =>{
       console.log(error);
     })
