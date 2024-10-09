89aa06729f3b9b74d9008730ada02419c2b2c3a6
Alex <alexnicolashoyos@gmail.com>
2024-10-08 23:34:39 -0300

    Añadido eliminarProducto

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { ProductoService } from '../../services/producto.service';
 import { Producto } from '../../models/producto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-listar-productos',
@@ -11,7 +12,8 @@ import { Producto } from '../../models/producto';
 export class ListarProductosComponent implements OnInit{
 
   listarProducto : Producto[] = [];
-  constructor(private _productoService: ProductoService  ) { }
+  constructor(private _productoService: ProductoService,
+        private toastr: ToastrService ) { }
 
   ngOnInit(): void {
     this.obtenerProductos();
@@ -22,6 +24,15 @@ export class ListarProductosComponent implements OnInit{
       console.log(data);
       this.listarProducto = data;
     }, error =>{
+      console.log(error);
+    })
+  }
+
+  eliminarProducto(id:any){
+    this._productoService.eliminarProducto(id).subscribe(data => {
+      this.toastr.error('El producto ha sido eliminado con exito', 'Producto eliminado');
+      this.obtenerProductos();
+    }, error => {
       console.log(error);
     })
   }
